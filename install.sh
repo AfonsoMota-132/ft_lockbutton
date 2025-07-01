5d6f35e41766ebc7095f6b66a1c928faf91d91e2
Afonso Goncalves Mota <afogonca@c1r5s7.42porto.com>
2025-07-01 12:16:03 +0100

    res

--- install.sh
+++ install.sh
@@ -30,7 +30,8 @@ if [ -d "$HOME/.local/share/gnome-shell/" ]; then
 	  echo "Please do Alt + F2, its gonna pop a menu, then write r and press enter"
 	  echo "Your GUI might freak out a bit, thats ok, its GNOME Shell restarting!"
 	  echo "If after restarting the button still doesn't appear
-	  echo "run this 'bash -c \"\$(curl -fsSL https://raw.githubusercontent.com/AfonsoMota-132/ft_lockbutton/main/run.sh)\"'"
+	  echo -n "run this bash -c" 
+	  echo '$(curl -fsSL https://raw.githubusercontent.com/AfonsoMota-132/ft_lockbutton/main/run.sh)'
 	  echo "cmd again"
 	  echo "If you prefer or if anything goes wrong"
 	  echo "you can contact afogonca for more help"
